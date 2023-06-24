4dc34cedaa3014a01082b4101c34e61bb5065fb5
Cameron Johnson <cameronrossjohnson@gmail.com>
2023-06-24 18:05:19 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Stable Diffusion Frontend
+      </header>
+    </div>
+  );
+}
+
+export default App;
