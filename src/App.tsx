7eed46d0cdc8ec84d11da0ccf06da72051561e35
Cameron Johnson <cameronrossjohnson@gmail.com>
2023-06-24 19:36:22 -0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import UserProfile from "./UserProfile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Stable Diffusion Frontend
+        <LoginButton />
+        <LogoutButton />
+        <UserProfile />
       </header>
     </div>
   );
